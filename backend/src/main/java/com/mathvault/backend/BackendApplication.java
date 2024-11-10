c5bc956bcd610a493698a9da3e480311ce1351b5
Anthony Mclamb <adylanmclamb@yahoo.com>
2024-11-10 07:24:00 -0500

    init

---
+++ backend/src/main/java/com/mathvault/backend/BackendApplication.java
@@ -0,0 +1,15 @@
+package com.mathvault.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		System.out.print("TESTING");
+
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
