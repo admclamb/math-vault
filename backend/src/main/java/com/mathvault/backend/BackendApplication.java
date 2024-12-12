79955edbc6a3e747eaff53698eab7ffb2c3a3540
Anthony Mclamb <adylanmclamb@yahoo.com>
2024-12-11 22:53:51 -0500

    init angular

--- backend/src/main/java/com/mathvault/backend/BackendApplication.java
+++ backend/src/main/java/com/mathvault/backend/BackendApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
 	public static void main(String[] args) {
-		System.out.print("TESTING");
-
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
