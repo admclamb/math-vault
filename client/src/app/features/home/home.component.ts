1dbe6f35f20bb228d07f46bfc023bd36c03b7059
Anthony Mclamb <adylanmclamb@yahoo.com>
2024-12-15 01:13:40 -0500

    setup landing

--- client/src/app/features/home/home.component.ts
+++ client/src/app/features/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { LayoutLandingComponent } from '../../components/layouts/layout-landing/layout-landing.component';
+import { HeroComponent } from './hero/hero.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [LayoutLandingComponent],
+  imports: [LayoutLandingComponent, HeroComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
